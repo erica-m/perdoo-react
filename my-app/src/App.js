270bf67fdd7a07fe50e578a21ee43184d8418c6b
Erica Schoonmaker <mail@designbyeri.ca>
2018-03-28 16:21:34 +0200

    Added click events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Loop from 'lloop';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Loop from "lloop";
 
 // Import icons
-import iconAdd from './add.svg'
-import iconSearch from './search.svg'
+import iconAdd from "./add.svg";
+import iconSearch from "./search.svg";
 
 /* CSS---------------------------------------------------------------------*/
 // Create a <Icon> react component that renders a <object> for SVG icons
@@ -16,13 +16,13 @@ const Icon = styled.object`
 // Create a <Wrapper> react component that renders a <section>
 const Wrapper = styled.section`
   font-family: "Proxima Nova", sans-serif;
-  color: #3E4A59;
+  color: #3e4a59;
   font-size: 15px;
   position: absolute;
   bottom: 0;
   width: 100%;
   background-color: rgba(202, 213, 220, 0.1);
-  border-top: 1px solid #E8EFF3;
+  border-top: 1px solid #e8eff3;
   padding: 15px 30px;
   display: flex;
   align-items: center;
@@ -34,8 +34,8 @@ const UserWrap = styled.div`
   align-items: center;
 
   &:hover {
-    background: #F9FAFB;
-    color: #059DDF;
+    background: #f9fafb;
+    color: #059ddf;
   }
 `;
 
@@ -44,14 +44,14 @@ const Popover = styled.div`
   position: absolute;
   bottom: 50px;
   left: 10px;
-  background: #FFFFFF;
+  background: #ffffff;
   border-radius: 4px;
-  box-shadow: 0 2px 4px #CAD5DC;
-  border: 1px solid #E8EFF3;
+  box-shadow: 0 2px 4px #cad5dc;
+  border: 1px solid #e8eff3;
 `;
 
 //Create a <UserList> react component that renders a <div>
-const UserList = styled.div `
+const UserList = styled.div`
   padding: 10px 20px;
   max-height: 150px;
   overflow: scroll;
@@ -62,7 +62,7 @@ const UserList = styled.div `
     &:last-child {
       margin-bottom: 0px;
     }
-  };
+  }
 `;
 
 // Create a <Avatar> react component that renders a <img>
@@ -76,7 +76,7 @@ const Avatar = styled.img`
 const Name = styled.div`
   margin: 0 8px;
   white-space: nowrap;
-  ${props => props.active && 'font-weight: 600;'}
+  ${props => props.active && "font-weight: 600;"}
   }
 `;
 
@@ -85,7 +85,7 @@ const SearchField = styled.input`
   height: 20px;
   border: 0;
   font-family: "Proxima Nova", sans-serif;
-  color: #3E4A59;
+  color: #3e4a59;
   font-size: 15px;
   margin-left: 8px;
 
@@ -96,7 +96,7 @@ const SearchField = styled.input`
 
 // Create a <Search> react component that renders a <div>
 const Search = styled.div`
-  border-top: 1px solid #E8EFF3;
+  border-top: 1px solid #e8eff3;
   padding: 10px 20px;
 
   > ${Icon} {
@@ -109,22 +109,22 @@ const Search = styled.div`
 
 /* Arrays------------------------------------------------------------------*/
 const users = [
-  { id: 1, name: 'Jessica Jones', imgUrl: 'https://randomuser.me/api/portraits/women/76.jpg' },
-  { id: 2, name: 'Luke Cage', imgUrl: 'https://randomuser.me/api/portraits/men/53.jpg' },
-  { id: 3, name: 'Matthew Murdock', imgUrl: 'https://randomuser.me/api/portraits/men/46.jpg' },
-  { id: 4, name: 'Elektra Natchios', imgUrl: 'https://randomuser.me/api/portraits/women/19.jpg' },
-  { id: 5, name: 'Trish Walker', imgUrl: 'https://randomuser.me/api/portraits/women/68.jpg' },
-  { id: 6, name: 'Franklin Nelson', imgUrl: 'https://randomuser.me/api/portraits/men/79.jpg' },
-  { id: 7, name: 'Claire Temple', imgUrl: 'https://randomuser.me/api/portraits/women/89.jpg' },
-  { id: 8, name: 'Daniel Rand', imgUrl: 'https://randomuser.me/api/portraits/men/62.jpg' },
-  { id: 9, name: 'Colleen Wing', imgUrl: 'https://randomuser.me/api/portraits/women/17.jpg' },
+  { id: 1, name: "Jessica Jones", imgUrl: "https://randomuser.me/api/portraits/women/76.jpg" },
+  { id: 2, name: "Luke Cage", imgUrl: "https://randomuser.me/api/portraits/men/53.jpg" },
+  { id: 3, name: "Matthew Murdock", imgUrl: "https://randomuser.me/api/portraits/men/46.jpg" },
+  { id: 4, name: "Elektra Natchios", imgUrl: "https://randomuser.me/api/portraits/women/19.jpg" },
+  { id: 5, name: "Trish Walker", imgUrl: "https://randomuser.me/api/portraits/women/68.jpg" },
+  { id: 6, name: "Franklin Nelson", imgUrl: "https://randomuser.me/api/portraits/men/79.jpg" },
+  { id: 7, name: "Claire Temple", imgUrl: "https://randomuser.me/api/portraits/women/89.jpg" },
+  { id: 8, name: "Daniel Rand", imgUrl: "https://randomuser.me/api/portraits/men/62.jpg" },
+  { id: 9, name: "Colleen Wing", imgUrl: "https://randomuser.me/api/portraits/women/17.jpg" }
 ];
 /*-------------------------------------------------------------------------*/
 
 /* Functions---------------------------------------------------------------*/
-const UserBlock = ({ name, imgUrl }) => (
-  <UserWrap>
-    <Avatar src={imgUrl} alt="user avatar"></Avatar>
+const UserBlock = ({ id, name, imgUrl, onClick }) => (
+  <UserWrap data-id={id} onClick={onClick}>
+    <Avatar src={imgUrl} alt="user avatar" />
     <Name>{name}</Name>
   </UserWrap>
 );
@@ -136,12 +136,26 @@ class App extends Component {
     super();
 
     this.state = {
-      filter: ""
+      open: false,
+      filter: "",
+      selectedUser: users[0]
     };
 
     this.onFilterUpdate = event => {
       this.setState({ filter: event.target.value });
     };
+
+    this.onSelectUser = user => event => {
+      this.setState({
+        open: false,
+        filter: "",
+        selectedUser: user
+      });
+    };
+
+    this.clickableUserBlock = user => UserBlock({ ...user, onClick: this.onSelectUser(user) });
+
+    this.openPopover = () => this.setState({ open: true });
   }
 
   filteredUsers() {
@@ -149,24 +163,28 @@ class App extends Component {
   }
 
   render() {
+    const selectedUser = this.state.selectedUser;
+    const popover = this.state.open ? (
+      <Popover>
+        <UserList onClick={this.onClick}>
+          <Loop items={this.filteredUsers()} primaryKey="id">
+            <this.clickableUserBlock />
+          </Loop>
+        </UserList>
+        <Search>
+          <Icon type="image/svg+xml" data={iconSearch} />
+          <SearchField placeholder="Search People" onChange={this.onFilterUpdate} autofocus />
+        </Search>
+      </Popover>
+    ) : (
+      []
+    );
+
     return (
       <Wrapper>
-
-        <Popover>
-          <UserList>
-            <Loop items={this.filteredUsers()} primaryKey="id">
-              <UserBlock />
-            </Loop>
-          </UserList>
-          <Search>
-            <Icon type="image/svg+xml" data={iconSearch}></Icon>
-            <SearchField placeholder="Search People" onChange={this.onFilterUpdate} ></SearchField>
-          </Search>
-        </Popover>
-
-        <UserBlock active imgUrl="https://randomuser.me/api/portraits/women/9.jpg" name="Erica Schoonmaker" />
-        <Icon type="image/svg+xml" data={iconAdd}></Icon>
-
+        {popover}
+        <UserBlock active imgUrl={selectedUser.imgUrl} name={selectedUser.name} onClick={this.openPopover} />
+        <Icon type="image/svg+xml" data={iconAdd} />
       </Wrapper>
     );
   }
